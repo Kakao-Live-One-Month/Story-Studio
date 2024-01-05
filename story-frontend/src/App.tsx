76dac65361a80aebfce4edded058fd4f91f5c6b7
송주하 <sjh990715@gmail.com>
2024-01-05 18:07:53 +0900

    fix: react.js + typescript + tailwindcss 프로젝트 생성 및 모듈 설치, 프로젝트 시작

---
+++ story-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-6xl text-center text-blue-400'>tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
