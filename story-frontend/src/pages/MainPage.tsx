97b45d5e9a86f4456767fd31967302bcff9d8bdc
eehij <a_se1020@naver.com>
2024-01-15 11:19:40 +0900

    issue-23 4

--- story-frontend/src/pages/MainPage.tsx
+++ story-frontend/src/pages/MainPage.tsx
@@ -14,15 +14,15 @@ const MainPage: React.FC = () => {
         <div className="font-sans relative w-full">
           <GenreProvider>
             <header className="pt-16 pb-8 text-black text-center relative h-[80vh] flex flex-col justify-center items-center">
-            <div
-              className="absolute top-0 left-0 w-full h-full bg-cover bg-center z-[-1]"
-              style={{
-                backgroundImage: 'url("../../img/headerbg.png")',
-              }}
-            />
-            <Link to="/" className="absolute top-0 left-0 p-5">
-                <img src="../../img/logo.png" alt="Image" className="cursor-pointer w-20 h-20 md:w-24 md:h-24 lg:w-32 lg:h-32 "/>
-              </Link>
+              <div
+                className="absolute top-0 left-0 w-full h-full bg-cover bg-center z-[-1]"
+                style={{
+                  backgroundImage: 'url("../../img/headerbg.png")',
+                }}
+              />
+              <Link to="/" className="absolute top-0 left-0 p-5">
+                  <img src="../../img/logo.png" alt="Image" className="cursor-pointer w-20 h-20 md:w-24 md:h-24 lg:w-32 lg:h-32 "/>
+                </Link>
               <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold">이야기 작업실</h1>
               <p className="text-xl text-gray-700 my-4 lg:my-8 lg:text-2xl">
                 작은 작업실에서 펼쳐내는 큰 꿈의 이야기
