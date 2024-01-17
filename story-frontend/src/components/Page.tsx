33185364eb77f6c0e0445e258de277d7acef3949
yunrry <128774803+yunrry@users.noreply.github.com>
2024-01-17 15:53:22 +0900

    동화 내용 줄바꿈

--- story-frontend/src/components/Page.tsx
+++ story-frontend/src/components/Page.tsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from 'react';
 import Image from '../components/Image';
 import { error } from 'console';
 
-
 // props의 타입을 정의하는 인터페이스
 interface PageProps {
   setStoryArray: React.Dispatch<React.SetStateAction<string[]>>;
@@ -25,30 +24,33 @@ const Page: React.FC<PageProps> = ({
   checkStoryCall
 }) => {
   const [showModal, setShowModal] = useState(false);
+  const [showLoading, setShowLoaging] = useState(false);
   const [currentPageStory, setCurrentPageStory] = useState<string>("undefind");
   const param = useParams();
   const page_id = Number(param.page_id);
   // console.log(page_id);  
 
 
+
   const currentText = async () => {
     try {
-      if (storyArray[page_id - 1]!== undefined) {
-        setCurrentPageStory(storyArray[page_id - 1]);
+      if (storyArray[page_id - 1] !== undefined) {
+        // 문장부호 뒤에 두 개의 줄바꿈 문자 추가 및 첫 줄에 스페이스 추가
+        const formattedStory = " " + storyArray[page_id - 1].replace(/([.?!])\s*/g, "$1\n\n\ ");
+        setCurrentPageStory(formattedStory);
       }
       else {
-        setCurrentPageStory("undefind");
+        setCurrentPageStory("undefined");
       }
     }
-     catch (error) {
+    catch (error) {
       console.error('이미지 생성 에러:', error);
     }
   };
 
-
   useEffect(() => {
     currentText();
-  }, [page_id, storyArray]); 
+  }, [page_id, storyArray]);
 
 
   return (
@@ -65,7 +67,10 @@ const Page: React.FC<PageProps> = ({
         </div>
 
         <div className="flex w-full ">
-          <p className='my-auto text-3xl p-10'>{currentPageStory}</p>
+          <p 
+            className='my-auto text-3xl p-10'
+            style={{ whiteSpace: 'pre-wrap' }}
+          >{currentPageStory}</p>
         </div>
       </div>
   );
